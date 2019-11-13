4ae2f38abc7339f61a299807f4e5eb9fd0654afa
Fabio Papa <fabtheman@gmail.com>
2019-11-13 13:12:07 -0800

    Add ability to log in/out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,21 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(email: params[:email])
+
+    # If the user exists AND the password entered is correct
+    if @user && @user.authenticate(params[:password])
+      # Save the user id in the cookie. This is how we keep the user logged in
+      session[:user_id] = @user.id
+      redirect_to '/', notice: 'Successfully logged in'
+    else
+      # If user's login doesn't work, send them back to the login form.
+      redirect_to '/login', notice: 'Wrong email or password. Please try again.'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to '/login', notice: 'You have been successfully logged out.'
   end
 end
