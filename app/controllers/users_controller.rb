c6d948b1ca7d293643d7675568abcf9a38d85397
Fabio Papa <fabtheman@gmail.com>
2019-11-13 12:26:11 -0800

    Log in the user after they create an account, and redirect to home

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,24 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to '/', notice: 'Created your account and logged you in!'
+    else
+      render '/signup'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :name,
+      :email,
+      :password,
+      :password_confirmation
+    )
   end
 end
