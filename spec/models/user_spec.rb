b42480e43de61b7ea3df22c76360cbc2decda546
Fabio Papa <fabtheman@gmail.com>
2019-11-18 15:23:38 -0800

    Implement password presence validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,28 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   context 'validation' do
-    it 'should pass with a valid user'
+    it 'should pass with a valid user' do
+      @user = User.new(
+        name: 'test user',
+        email: 'test@test.com',
+        password: 's3kr37',
+        password_confirmation: 's3kr37'
+      )
 
-    it 'should fail when password is blank'
+      expect(@user).to be_valid
+    end
+
+    it 'should fail when password is blank' do
+      @user = User.new(
+        name: 'test user',
+        email: 'test@test.com',
+        password: nil,
+        password_confirmation: 's3kr37'
+      )
+
+      @user.password = nil
+      expect(@user).to_not be_valid
+    end
 
     it 'should fail when password confirmation is blank'
 
