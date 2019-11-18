8c2886e303b6c6ea7b6c63d7c1ae0389d0d985b8
Fabio Papa <fabtheman@gmail.com>
2019-11-18 15:27:33 -0800

    Refactor user spec examples

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,27 +2,24 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   context 'validation' do
-    it 'should pass with a valid user' do
+    before(:each) do
       @user = User.new(
         name: 'test user',
         email: 'test@test.com',
         password: 's3kr37',
         password_confirmation: 's3kr37'
       )
+    end
 
+    it 'should pass with a valid user' do
       expect(@user).to be_valid
     end
 
     it 'should fail when password is blank' do
-      @user = User.new(
-        name: 'test user',
-        email: 'test@test.com',
-        password: nil,
-        password_confirmation: 's3kr37'
-      )
-
       @user.password = nil
       expect(@user).to_not be_valid
+      expect(@user.errors.full_messages_for(:password))
+        .to include(match(/blank/i))
     end
 
     it 'should fail when password confirmation is blank'
