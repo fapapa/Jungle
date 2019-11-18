bf42c16b18014cd02e7faee63e7b830ca21793d1
Fabio Papa <fabtheman@gmail.com>
2019-11-18 14:14:25 -0800

    Add pending example stubs to user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validation' do
+    it 'should pass with a valid user'
+
+    it 'should fail when password is blank'
+
+    it 'should fail when password confirmation is blank'
+
+    it 'should fail when password confirmation does not match'
+
+    it 'should fail when email already exists (case insensitive)'
+
+    it 'should fail when email is blank'
+
+    it 'should fail when first name is blank'
+
+    it 'should fail when last name is blank'
+  end
 end
