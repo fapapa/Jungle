8216f3f8c964fb1591afc69a3820e409db474755
Fabio Papa <fabtheman@gmail.com>
2019-11-18 13:15:54 -0800

    Add specs for a valid product and a product missing a name

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,9 +2,24 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'validations' do
-    it 'should be valid when all required fields are present'
+    before(:each) do
+      category = Category.create(name: "Clothing")
+      @product = category.products.new(
+        name: "Men's Denim Overalls",
+        price: 127.95,
+        quantity: 23,
+      )
+    end
 
-    it 'should not be valid when name is mising'
+    it 'should be valid when all required fields are present' do
+      expect(@product).to be_valid
+    end
+
+    it 'should not be valid when name is mising' do
+      @product.name = nil
+
+      expect(@product).to_not be_valid
+    end
 
     it 'should not be valid when price is missing'
 
