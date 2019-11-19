11e5cba0d173d1242f9ce9b8da8b7e4b3ac52ba0
Fabio Papa <fabtheman@gmail.com>
2019-11-18 20:23:05 -0800

    Change login to use new User.authenticate_with_credentials method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,15 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: params[:email])
+    user = User.authenticate_with_credentials(
+      params[:email],
+      params[:password]
+    )
 
-    # If the user exists AND the password entered is correct
-    if @user && @user.authenticate(params[:password])
-      # Save the user id in the cookie. This is how we keep the user logged in
-      session[:user_id] = @user.id
+    if user
+      session[:user_id] = user.id
       redirect_to '/', notice: 'Successfully logged in'
     else
-      # If user's login doesn't work, send them back to the login form.
       redirect_to '/login', notice: 'Wrong email or password. Please try again.'
     end
   end
