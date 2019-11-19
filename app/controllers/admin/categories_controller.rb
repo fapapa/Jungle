eae9d31ae91015dc89c07ae830d24ff6073e244e
Fabio Papa <fabtheman@gmail.com>
2019-11-19 12:40:28 -0800

    Protect admin/categories with http_basic

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,7 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV['HTTP_BASIC_USERNAME'],
+                               password: ENV['HTTP_BASIC_PASSWORD']
+
   def index
     @categories = Category.all
   end
